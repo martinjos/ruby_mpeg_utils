770d1f18fd655faf0b9cba0c4a96d327a228e31d
Martin Sidaway <msidaway@gmail.com>
2013-09-12 14:24:35 +0100

    Generalise reading of box info to also work with nested boxes (hopefully). Fix bug in reading extended size.

--- mpeg.rb
+++ mpeg.rb
@@ -1,31 +1,38 @@
 #!/usr/bin/env ruby
 
 fname = ARGV[0]
-boxes = Hash.new{|h,k| h[k] = [] }
 
-File.open(fname) {|f|
-	fsize = f.size
-	pos = 0
-	while pos <= fsize - 8
+def get_box_info(f, start=0, size=f.size)
+	boxes = Hash.new{|h,k| h[k] = [] }
+
+	pos = start
+	end_pos = start + size
+
+	while pos <= end_pos - 8
+		f.seek(pos)
 		bsize = f.read(4).unpack('L>')[0]
 		type = f.read(4)
 		pos += 8
 		if bsize == 0
 			puts "Size in quad"
-			bsize = f.read(8).unpack('Q>') - 16
+			bsize = f.read(8).unpack('Q>')[0] - 16
 			pos += 8
 		elsif bsize == 1
-			puts "To EOF"
-			bsize = fsize - pos
+			puts "To end_pos"
+			bsize = end_pos - pos
 		else
 			bsize -= 8
 		end
-		puts "Got box size=#{bsize}, type=#{type.inspect}"
+		puts "Got box pos=#{pos} size=#{bsize}, type=#{type.inspect}"
 		boxes[type] << [pos, bsize]
 		pos += bsize
-		f.seek(pos)
 	end
-	puts "Ended at pos=#{pos}, fsize=#{fsize}, diff=#{fsize-pos}"
+	puts "Ended at pos=#{pos}, end_pos=#{end_pos}, diff=#{end_pos-pos}"
+	boxes
+end
+
+File.open(fname) {|f|
+	boxes = get_box_info(f)
 
 	get_box = lambda {|array|
 		f.seek(array[0])
